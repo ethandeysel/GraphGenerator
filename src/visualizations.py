c8c5d0742ee132af5d371eb8395d024cc0c5a5b3
Ethan Deysel <ethandey27@gmail.com>
2026-02-28 20:03:06 +0200

    Added Graph legends

--- src/visualizations.py
+++ src/visualizations.py
@@ -18,9 +18,11 @@ def get_pillar_chart(row_data, averages):
     for i, col in enumerate(cols):
         ax.hlines(avgs[col], i - 0.4, i + 0.4, colors='red', linewidth=2)
         ax.text(i, row_val[col] / 2, f"{row_val[col]:.1f}", ha='center', fontsize=10)
-
+    bar_legend = [mpatches.Patch(color=bar_colours[i], label=cols[i]) for i in range(7)]
+    avg_patch = mpatches.Patch(color='red', label='Group Average')
     ax.set_title(f"{row_data['SUPPLIER']} Pillar Scores vs Group Average")
     ax.set_ylim(0, 105)
+    plt.legend(handles=[avg_patch] + bar_legend, loc='center left', fontsize=15, bbox_to_anchor=(1.02, 0.5))
     ax.set_xticks(range(7))
     ax.set_xticklabels(['A', 'B', 'C', 'D', 'E', 'F', 'G'])
     
@@ -36,8 +38,18 @@ def get_risk_bar(row_data, avg_risk):
         if x < 3: return '#FFB742'
         if x < 4: return '#8AFF93'
         return '#26C700'
-
+    avg_patch = mpatches.Patch(color='red', label='Group Average')
+    color_legend = [
+        mpatches.Patch(color='#26C700', label='Low'),
+        mpatches.Patch(color='#8AFF93', label='Low-Medium'),
+        mpatches.Patch(color='#FFB742', label='Medium'),
+        mpatches.Patch(color='#FF908A', label='High'),
+        mpatches.Patch(color='#ED009F', label='Critical')
+    ]
     fig, ax = plt.subplots(figsize=(10, 2))
+    plt.text(1*val/2,0, f"{val:.2f}", ha='center', va='center', fontsize=9)
+    plt.legend(handles=[avg_patch] + color_legend,
+               loc='center left', bbox_to_anchor=(1.02, 0.5), fontsize=9)
     ax.barh(['Risk Rating'], [val], color=get_color(val), height=0.4)
     ax.axvline(avg_risk, color='red', linewidth=2, label='Avg')
     ax.set_xlim(0, 5)
